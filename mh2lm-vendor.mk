174dc32c5e7b0e20a6770ac6da3b5e03ae9eb989
Han Sol Jin <hansol@hansol.ca>
2024-09-10 02:55:40 -0700

    mh2lm: drop libsnsapi.so
    Change-Id: Idf104f588055df23e88ba174860537bc39b5a00a

--- mh2lm-vendor.mk
+++ mh2lm-vendor.mk
@@ -615,7 +615,6 @@ PRODUCT_COPY_FILES += \
     vendor/lge/mh2lm/proprietary/vendor/lib/libmorpho_image_stab31.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmorpho_image_stab31.so \
     vendor/lge/mh2lm/proprietary/vendor/lib/libmorpho_wdr2.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmorpho_wdr2.so \
     vendor/lge/mh2lm/proprietary/vendor/lib/libmpbase.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmpbase.so \
-    vendor/lge/mh2lm/proprietary/vendor/lib/libsnsapi.so:$(TARGET_COPY_OUT_VENDOR)/lib/libsnsapi.so \
     vendor/lge/mh2lm/proprietary/vendor/lib/libspectre3.so:$(TARGET_COPY_OUT_VENDOR)/lib/libspectre3.so \
     vendor/lge/mh2lm/proprietary/vendor/lib/libsymphony-1.1.2.so:$(TARGET_COPY_OUT_VENDOR)/lib/libsymphony-1.1.2.so \
     vendor/lge/mh2lm/proprietary/vendor/lib/libsymphony-1.1.4.so:$(TARGET_COPY_OUT_VENDOR)/lib/libsymphony-1.1.4.so \
@@ -865,7 +864,6 @@ PRODUCT_COPY_FILES += \
     vendor/lge/mh2lm/proprietary/vendor/lib64/libmorpho_image_stab31.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libmorpho_image_stab31.so \
     vendor/lge/mh2lm/proprietary/vendor/lib64/libmorpho_wdr2.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libmorpho_wdr2.so \
     vendor/lge/mh2lm/proprietary/vendor/lib64/libmpbase.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libmpbase.so \
-    vendor/lge/mh2lm/proprietary/vendor/lib64/libsnsapi.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libsnsapi.so \
     vendor/lge/mh2lm/proprietary/vendor/lib64/libspectre3.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libspectre3.so \
     vendor/lge/mh2lm/proprietary/vendor/lib64/libsymphony-1.1.4.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libsymphony-1.1.4.so \
     vendor/lge/mh2lm/proprietary/vendor/lib64/libsymphony-cpu.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libsymphony-cpu.so \
